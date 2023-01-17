9b3a74d2dc051e52c446d7073c72070e693e7c63
Dmitriy Stavnichuk <42932219+yettimon@users.noreply.github.com>
2023-01-17 13:36:05 +0100

    initial commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+$(function() {
+  var card = $('.js-card');
+  var cardBaseWidth = card.width();
+  var arrow = $('.js-arrow');
+  var aspectRatio = 1;
+
+  card.resizable({
+    aspectRatio: aspectRatio,
+    handles: 'se',
+  });
+
+  card.on('resizestop', function() {
+    updateAllRings();
+  });
+
+  // list of ring sizes US
+  var rings = {
+    'ring-4': 56.15888688,
+    'ring-4-5': 57.71083555,
+    'ring-5': 59.33496789,
+    'ring-5-5': 60.85082473,
+    'ring-6': 62.4027734,
+    'ring-6-5': 63.95472208,
+    'ring-7': 65.57885441,
+    'ring-7-5': 67.08268064,
+    'ring-8': 68.75493542,
+    'ring-8-5': 70.03018006,
+    'ring-9': 71.38963898,
+    'ring-9-5':72.54457753,
+    'ring-10': 74.10855681,
+    'ring-10-5': 75.55222999,
+    'ring-11': 77.11620928,
+  };
+
+  function updateAllRings() {
+    var changeRatio = 1 + ((card.width() - cardBaseWidth) / cardBaseWidth);
+
+    for (var key in rings) {
+      var newDiameter = rings[key] * changeRatio;
+      $('.' + key).width(newDiameter).height(newDiameter);
+    }
+  }
+});
