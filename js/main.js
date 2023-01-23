8290af7ffb486ee2748d2e5f4c035cd78bc89a51
yettimonpricewise <dmytro.stavnichuk@pricewise.sk>
2023-01-23 12:22:55 +0100

    changed ring sizes from us to uk, updated coin for comparison

--- js/main.js
+++ js/main.js
@@ -1,43 +1,40 @@
-$(function() {
-  var card = $('.js-card');
+$(function () {
+  var card = $(".js-card");
   var cardBaseWidth = card.width();
-  var arrow = $('.js-arrow');
+  var arrow = $(".js-arrow");
   var aspectRatio = 1;
 
   card.resizable({
     aspectRatio: aspectRatio,
-    handles: 'se',
+    handles: "se",
   });
 
-  card.on('resizestop', function() {
+  card.on("resizestop", function () {
     updateAllRings();
   });
 
   // list of ring sizes US
   var rings = {
-    'ring-4': 56.15888688,
-    'ring-4-5': 57.71083555,
-    'ring-5': 59.33496789,
-    'ring-5-5': 60.85082473,
-    'ring-6': 62.4027734,
-    'ring-6-5': 63.95472208,
-    'ring-7': 65.57885441,
-    'ring-7-5': 67.08268064,
-    'ring-8': 68.75493542,
-    'ring-8-5': 70.03018006,
-    'ring-9': 71.38963898,
-    'ring-9-5':72.54457753,
-    'ring-10': 74.10855681,
-    'ring-10-5': 75.55222999,
-    'ring-11': 77.11620928,
+    "ring-J": 58.54488189,
+    "ring-K": 60.094488189,
+    "ring-L": 61.644094488,
+    "ring-M": 63.155905512,
+    "ring-N": 64.705511811,
+    "ring-O": 66.25511811,
+    "ring-P": 67.766929134,
+    "ring-Q": 69.316535433,
+    "ring-R": 70.866141732,
+    "ring-S": 72.377952756,
   };
 
   function updateAllRings() {
-    var changeRatio = 1 + ((card.width() - cardBaseWidth) / cardBaseWidth);
+    var changeRatio = 1 + (card.width() - cardBaseWidth) / cardBaseWidth;
 
     for (var key in rings) {
       var newDiameter = rings[key] * changeRatio;
-      $('.' + key).width(newDiameter).height(newDiameter);
+      $("." + key)
+        .width(newDiameter)
+        .height(newDiameter);
     }
   }
 });
